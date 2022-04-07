baf8062d24d5cd9e59b59f76ce2d460c83792cec
Pablo González <pablo.gonzalez@dynaccurate.com>
2022-04-07 18:04:23 -0300

    creating db model

--- schedule_app/core/admin.py
+++ schedule_app/core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from core.models import Event
 # Register your models here.
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'event_date', 'criation_date')
+    list_filter = ('title', 'user',)
+
+
+admin.site.register(Event, EventAdmin)
